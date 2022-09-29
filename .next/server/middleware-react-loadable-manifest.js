bc9577f7eb114b1c757619c59545670eade19383
Kyle Jones <102453624+lowlyphe@users.noreply.github.com>
2022-09-29 11:04:05 -0400

    Delete .next directory

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST={"_app.js -> bootstrap/dist/js/bootstrap.bundle.min.js":{"id":"_app.js -> bootstrap/dist/js/bootstrap.bundle.min.js","files":["static/chunks/node_modules_bootstrap_dist_js_bootstrap_bundle_min_js.js"]}}
