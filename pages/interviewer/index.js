4956c7293c8bd8263156af93b4f7880e05d2e006
Reaveskev <Reaveskev@gmail.com>
2022-09-30 12:27:23 -0700

    yo

--- pages/interviewer/index.js
+++ pages/interviewer/index.js
@@ -1,6 +1,6 @@
 import Header from "../../src/components/Header";
 import Footer from "../../src/components/Footer";
-import Body from "../../src/components/Body";
+
 import InterviewDash from "../../src/components/InterviewDash";
 
 export default function index() {
