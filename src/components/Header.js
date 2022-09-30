e371200070862ea6dbeb7d8fecc472c7d67bedeb
Gage Lewis <glewis.ghl@gmail.com>
2022-10-01 00:14:08 +0900

    fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,30 +45,13 @@ function Header() {
 
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="/interviewer">
-                  Interview
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/dashboard">Dashboard</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div className={styles.header}>
+        <div className={styles.logo}></div>
 
+        <p className={styles.para}>{currentPage}</p>
+        <p className={styles.para2}>{today}</p>
+        <p className={styles.para3}>{currentUser}</p>
+      </div>
     </>
   );
 }
