4260154620b41286d724fd240d0b884b160cc03c
lowlyphe <jones.kyle2893@gmail.com>
2022-09-30 15:27:52 -0400

    working with CE

--- pages/interview/index.js
+++ pages/interview/index.js
@@ -2,6 +2,7 @@ import CodeEditor from "../../src/components/CodeEditor"
 import Dashboard from '../../src/components/Dashboard'
 import Header from '../../src/components/Header'
 import Footer from "../../src/components/Footer"
+
 function id({ id }) {
     
     return (
