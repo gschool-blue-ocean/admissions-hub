ab317679e48474c3c00f3e2ee6295dbb8577f5de
Gglws <107443072+Gglws@users.noreply.github.com>
2022-10-05 04:17:23 +0900

    Update index.js

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,7 +1,7 @@
 import Header from "../../src/components/Header";
 import Footer from "../../src/components/Footer";
 import InterviewDash from "../../src/components/InterviewDash";
-import Problems from "../../src/components/Problems";
+
 
 export default function index() {
   return (
