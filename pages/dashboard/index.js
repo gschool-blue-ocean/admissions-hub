c1f0b86ef5de1bd31aec1b7966db09fc874a6904
Gage Lewis <glewis.ghl@gmail.com>
2022-10-05 04:18:25 +0900

    Markdown text boxes

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,7 +1,6 @@
 import Header from "../../src/components/Header";
 import Footer from "../../src/components/Footer";
 import InterviewDash from "../../src/components/InterviewDash";
-import Problems from "../../src/components/Problems";
 
 export default function index() {
   return (
