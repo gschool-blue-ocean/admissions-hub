7e7d96e3455ea5b4c7c98a26afea7cac0844fc65
Gage Lewis <glewis.ghl@gmail.com>
2022-10-05 04:15:13 +0900

    Markdown text boxes

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,13 @@
 import Header from "../../src/components/Header";
 import Footer from "../../src/components/Footer";
 import InterviewDash from "../../src/components/InterviewDash";
+import Problems from "../../src/components/Problems";
 
 export default function index() {
   return (
     <div>
       <Header />
+      <Problems />
       <InterviewDash />
       <Footer />
     </div>
