849b9bd27268bc42f28144bdea8295442335e4f4
HungNguyen16 <HungNguyen16@github.com>
2022-10-12 13:23:47 -0500

    fixed login navbar

---
+++ src/components/HeaderLogin.js
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import styles from "./Header.module.css";
+import Image from "next/image";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useRouter } from "next/router";
+import BtnLogin from "./BtnLogin";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+
+function HeaderLogin() {
+  let currentPage = "";
+  let currentUser = "";
+  let { asPath } = useRouter();
+  //get current access token from local storage
+  useEffect(() => {
+    let accessToken = localStorage.getItem("accessToken");
+
+    switch (asPath) {
+      case `/dashboard?access=${accessToken}`:
+        currentPage = "Interview Dashboard";
+        currentUser = "Danny Andrews";
+        break;
+      case "/login":
+        currentPage = "Interview Login";
+        currentUser = "";
+        break;
+    }
+  }, []);
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = monthNames[today.getMonth()];
+  var yyyy = today.getFullYear();
+  today = mm + ", " + dd + " " + yyyy;
+
+  const router = useRouter();
+  function goLink(event, data) {
+    if (event.target.name === "dashboard") {
+      router.push("../dashboard");
+    }
+  }
+
+  return (
+    <>
+      <div className={styles.header}>
+        {asPath !== "/login" ? (
+          <button
+            className={styles.logo}
+            onClick={goLink}
+            name="dashboard"
+          ></button>
+        ) : (
+          <button className={styles.logo} name="dashboard"></button>
+        )}
+
+        <div className={styles.para}>
+          {asPath !== "/login" ? (
+            <div className={styles.innerPara}>{currentPage}</div>
+          ) : (
+            <div className={styles.innerLoginPara}>{currentPage}</div>
+          )}
+        </div>
+        <div className={styles.para2}>{today}</div>
+      </div>
+    </>
+  );
+}
+
+export default HeaderLogin;
