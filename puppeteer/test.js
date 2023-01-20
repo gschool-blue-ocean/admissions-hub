3a32d318198a657c987350271bd8034c42f80d99
Fernando castro <fernandocastro77@yahoo.com>
2023-01-20 15:08:06 -0800

    working on innerHTML test"

--- puppeteer/test.js
+++ puppeteer/test.js
@@ -1,10 +1,18 @@
 const puppeteer = require("puppeteer");
 const pageUrl = "http://localhost:3000/login";
 
+//just taking a screenshot of the login page
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(pageUrl);
   await page.screenshot({ path: "loginpage.png" });
+
+  //   const grabTechologies = await page.evaluate(() => {
+  //     const
+  //     const pgTag = document.querySelectorAll(".Header_para2__OCRsC div");
+  //     return pgTag.innerHTML;
+  //   });
+  //   console.log(grabTechologies);
   await browser.close();
 })();
