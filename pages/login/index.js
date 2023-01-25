73de49ce6e67e064ed459c1a071d71b040e0169b
ghribik <g.hribik@gmail.com>
2023-01-25 12:39:25 -0800

    changed 'class' to 'className' to stop error message

--- pages/login/index.js
+++ pages/login/index.js
@@ -34,7 +34,7 @@ export default function Login() {
         className="text-center text-lg-start text-muted"
       >
         <div
-          class="fixed-bottom text-center p-4"
+          className="fixed-bottom text-center p-4"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
         >
           © 2023 Copyright: Team 3
