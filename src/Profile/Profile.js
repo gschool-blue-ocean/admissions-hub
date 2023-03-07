b4a3cc4ab3137afe9d16ce2262fbe69c26eb83c8
Sangyeon Pak <sangyeonpak1994@gmail.com>
2023-03-07 15:38:03 -0500

    after wsl reinstall

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import classes from '../../styles/Profile.module.css';
-
+//test commit
 function ProfilePage() {
   ///////////////////////////// state variables/////////////////////////////
   const [firstName, setFirstName] = useState('');
