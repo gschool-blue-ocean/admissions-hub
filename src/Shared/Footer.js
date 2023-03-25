7eec85389eb7cf169f342bf4b5eb5cbc2818f6ee
kmnorth96 <kmnorth96@gmail.com>
2023-03-24 22:31:24 -0700

    Updated footer and tooltip to URL Copier

--- src/Shared/Footer.js
+++ src/Shared/Footer.js
@@ -1,5 +1,37 @@
-import styles from '../../styles/Shared.module.css';
+import styles from "../../styles/Shared.module.css";
 
 export default function Footer() {
-  return <div className={styles.footer}>© 2023 Copyright: Team 3</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>© 2023 GitClub</div>
+      <ul className={styles.footerList}>
+        <li>
+          <a
+            className={styles.footerItem}
+            href="http://www.galvanize.com/privacy"
+          >
+            PRIVACY POLICY
+          </a>
+        </li>
+        <li>
+          <a
+            className={styles.footerItem}
+            href="http://www.galvanize.com/terms-of-use"
+          >
+            TERMS OF USE
+          </a>
+        </li>
+        <li>
+          <a className={styles.footerItem} href="http://www.galvanize.com">
+            Galvanize
+          </a>
+        </li>
+        <li>
+          <a className={styles.footerItem} href="mailto:info@galvanize.com">
+            info@galvanize.com
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
